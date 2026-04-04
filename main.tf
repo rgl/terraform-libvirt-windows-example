f94586643570e1b1d66eb6302d49602544b87fdd
Rui Lopes <rgl@ruilopes.com>
2026-04-04 18:52:27 +0100

    WIP upgrade to dmacvicar/libvirt 0.9.7
    see https://github.com/dmacvicar/terraform-provider-libvirt/issues/1323

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
     # see https://github.com/dmacvicar/terraform-provider-libvirt
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.8.3"
+      version = "0.9.7"
     }
   }
 }
@@ -56,33 +56,55 @@ variable "network_cidr" {
   default = "10.17.5.0/24"
 }
 
-locals {
-  ip_address = cidrhost(var.network_cidr, 2)
-}
-
 # NB this generates a single random number for the cloud-init instance-id.
 resource "random_id" "example" {
   byte_length = 10
 }
 
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.8.3/website/docs/r/network.markdown
+# see https://gitlab.com/libosinfo/osinfo-db/-/blob/main/data/os/microsoft.com/win-2k22.xml.in
+# see https://gitlab.com/libosinfo/osinfo-db/-/blob/main/data/os/microsoft.com/win-2k25.xml.in
+# see https://gitlab.com/libosinfo/osinfo-db/-/blob/main/data/os/microsoft.com/win-11.xml.in
+locals {
+  windows_version = regex("windows-([^-]+)", var.base_volume_name)[0]
+  windows_version_to_os_map = {
+    "2022" = "2k22"
+    "2025" = "2k25"
+    "11"   = "11"
+  }
+  os_id = "http://microsoft.com/win/${lookup(local.windows_version_to_os_map, local.windows_version, "2k22")}"
+}
+
+# see https://registry.terraform.io/providers/dmacvicar/libvirt/0.9.7/docs/resources/network
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.9.7/docs/resources/network.md
 resource "libvirt_network" "example" {
-  name      = var.prefix
-  mode      = "nat"
-  domain    = "example.test"
-  addresses = [var.network_cidr]
-  dhcp {
-    enabled = false
+  name = var.prefix
+  forward = {
+    nat = {
+      ports = [
+        {
+          start = 1024
+          end   = 65535
+        }
+      ]
+    }
   }
-  dns {
-    enabled    = true
-    local_only = false
+  domain = {
+    name = "example.test"
   }
-  lifecycle {
-    ignore_changes = [
-      dhcp[0].enabled, # see https://github.com/dmacvicar/terraform-provider-libvirt/issues/998
-    ]
-  }
+  ips = [
+    {
+      address = cidrhost(var.network_cidr, 1)
+      netmask = cidrnetmask(var.network_cidr)
+      dhcp = {
+        ranges = [
+          {
+            start = cidrhost(var.network_cidr, 2)
+            end   = cidrhost(var.network_cidr, -2)
+          }
+        ]
+      }
+    }
+  ]
 }
 
 # a multipart cloudbase-init cloud-config.
@@ -178,8 +200,9 @@ data "cloudinit_config" "example" {
 
 # a cloudbase-init cloud-config disk.
 # NB this creates an iso image that will be used by the NoCloud cloudbase-init datasource.
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.8.3/website/docs/r/cloudinit.html.markdown
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.8.3/libvirt/cloudinit_def.go#L139-L168
+# see https://registry.terraform.io/providers/dmacvicar/libvirt/0.9.7/docs/resources/cloudinit_disk
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.9.7/docs/resources/cloudinit_disk.md
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.9.7/internal/provider/cloudinit_disk_resource.go#L291-L341
 resource "libvirt_cloudinit_disk" "example_cloudinit" {
   name = "${var.prefix}-cloudinit.iso"
   meta_data = jsonencode({
@@ -188,55 +211,221 @@ resource "libvirt_cloudinit_disk" "example_cloudinit" {
   user_data = data.cloudinit_config.example.rendered
 }
 
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.8.3/website/docs/r/volume.html.markdown
+# see https://registry.terraform.io/providers/dmacvicar/libvirt/0.9.7/docs/resources/volume
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.9.7/docs/resources/volume.md
+resource "libvirt_volume" "example_cloudinit" {
+  pool = "default"
+  name = "${var.prefix}-cloudinit.iso"
+  create = {
+    content = {
+      url = libvirt_cloudinit_disk.example_cloudinit.path
+    }
+  }
+}
+
+# this uses the vagrant debian image imported from https://github.com/rgl/debian-vagrant.
+# see https://registry.terraform.io/providers/dmacvicar/libvirt/0.9.7/docs/resources/volume
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.9.7/docs/resources/volume.md
 resource "libvirt_volume" "example_root" {
-  name             = "${var.prefix}-root.img"
-  base_volume_name = var.base_volume_name
-  format           = "qcow2"
-  size             = 66 * 1024 * 1024 * 1024 # 66GiB. this root FS is automatically resized by cloudbase-init (by its cloudbaseinit.plugins.windows.extendvolumes.ExtendVolumesPlugin plugin which is included in the rgl/windows-vagrant image).
+  pool     = "default"
+  name     = "${var.prefix}-root.img"
+  capacity = 66 * 1024 * 1024 * 1024 # 66GiB. this root FS is automatically resized by cloudbase-init (by its cloudbaseinit.plugins.windows.extendvolumes.ExtendVolumesPlugin plugin which is included in the rgl/windows-vagrant image).
+  target = {
+    format = {
+      type = "qcow2"
+    }
+  }
+  backing_store = {
+    format = {
+      type = "qcow2"
+    }
+    path = "/var/lib/libvirt/images/${var.base_volume_name}"
+  }
 }
 
 # a data disk.
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.8.3/website/docs/r/volume.html.markdown
+# see https://registry.terraform.io/providers/dmacvicar/libvirt/0.9.7/docs/resources/volume
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.9.7/docs/resources/volume.md
 resource "libvirt_volume" "example_data" {
-  name   = "${var.prefix}-data.img"
-  format = "qcow2"
-  size   = 6 * 1024 * 1024 * 1024 # 6GiB.
+  pool     = "default"
+  name     = "${var.prefix}-data.img"
+  capacity = 6 * 1024 * 1024 * 1024 # 6GiB.
+  target = {
+    format = {
+      type = "qcow2"
+    }
+  }
 }
 
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.8.3/website/docs/r/domain.html.markdown
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.9.7/website/docs/r/domain.html.markdown
 resource "libvirt_domain" "example" {
   name        = var.prefix
   description = "created from ${path.cwd}"
-  machine     = "q35"
-  firmware    = "/usr/share/OVMF/OVMF_CODE_4M.fd"
-  cpu {
+  running     = true
+  type        = "kvm"
+  vcpu        = 2
+  memory      = 1024
+  memory_unit = "MiB"
+  features = {
+    acpi = true
+    apic = {}
+    pae  = true
+  }
+  metadata = {
+    xml = <<-EOF
+      <libosinfo:libosinfo xmlns:libosinfo="http://libosinfo.org/xmlns/libvirt/domain/1.0">
+        <libosinfo:os id="${local.os_id}"/>
+      </libosinfo:libosinfo>
+      EOF
+  }
+  os = {
+    type         = "hvm"
+    type_arch    = "x86_64"
+    type_machine = "q35"
+    firmware     = "efi"
+  }
+  cpu = {
     mode = "host-passthrough"
   }
-  vcpu   = 2
-  memory = 1024
-  video {
-    type = "qxl"
+  devices = {
+    graphics = [
+      {
+        spice = {
+          auto_port = true
+          listeners = [
+            {
+              address = {}
+            }
+          ]
+        }
+      }
+    ]
+    videos = [
+      {
+        model = {
+          type    = "qxl"
+          primary = "yes"
+          vram    = 65536
+          ram     = 65536
+          vga_mem = 16384
+          heads   = 1
+        }
+      }
+    ]
+    controllers = [
+      {
+        type  = "scsi"
+        model = "virtio-scsi"
+      },
+      {
+        type = "virtio-serial"
+      }
+    ]
+    channels = [
+      {
+        source = {
+          unix = {
+            mode = "bind"
+          }
+        }
+        target = {
+          virt_io = {
+            name = "org.qemu.guest_agent.0"
+          }
+        }
+      },
+      {
+        source = {
+          spice_vmc = true
+        }
+        target = {
+          virt_io = {
+            name = "com.redhat.spice.0"
+          }
+        }
+      }
+    ]
+    rngs = [
+      {
+        model = "virtio"
+        backend = {
+          random = "/dev/urandom"
+        }
+      }
+    ]
+    disks = [
+      {
+        driver = {
+          name = "qemu"
+          type = "qcow2"
+        }
+        source = {
+          volume = {
+            pool   = libvirt_volume.example_root.pool
+            volume = libvirt_volume.example_root.name
+          }
+        }
+        target = {
+          bus = "scsi"
+          dev = "sda"
+        }
+        wwn = format("000000000000aa%02x", 0)
+      },
+      {
+        driver = {
+          name = "qemu"
+          type = "qcow2"
+        }
+        source = {
+          volume = {
+            pool   = libvirt_volume.example_data.pool
+            volume = libvirt_volume.example_data.name
+          }
+        }
+        target = {
+          bus = "scsi"
+          dev = "sdb"
+        }
+        wwn = format("000000000000ab%02x", 0)
+      },
+      {
+        device = "cdrom"
+        source = {
+          volume = {
+            pool   = libvirt_volume.example_cloudinit.pool
+            volume = libvirt_volume.example_cloudinit.name
+          }
+        }
+        target = {
+          bus = "scsi"
+          dev = "hdd"
+        }
+        serial = "cloudinit"
+      }
+    ]
+    interfaces = [
+      {
+        type = "network"
+        model = {
+          type = "virtio"
+        }
+        source = {
+          network = {
+            network = libvirt_network.example.name
+          }
+        }
+        # TODO https://github.com/dmacvicar/terraform-provider-libvirt/issues/1323
+        wait_for_ip = {
+          source  = "agent"
+          timeout = 300 # 300s (5m).
+        }
+      }
+    ]
   }
-  xml {
-    xslt = file("libvirt-domain.xsl")
-  }
-  qemu_agent = true
-  cloudinit  = libvirt_cloudinit_disk.example_cloudinit.id
-  disk {
-    volume_id = libvirt_volume.example_root.id
-    scsi      = true
-  }
-  disk {
-    volume_id = libvirt_volume.example_data.id
-    scsi      = true
-  }
-  network_interface {
-    network_id     = libvirt_network.example.id
-    hostname       = "example"
-    addresses      = [local.ip_address]
-    wait_for_lease = true
-  }
+}
+
+# see https://developer.hashicorp.com/terraform/language/resources/terraform-data
+resource "terraform_data" "example_provision" {
   provisioner "remote-exec" {
     inline = [
       <<-EOF
@@ -253,19 +442,18 @@ resource "libvirt_domain" "example" {
       type     = "winrm"
       user     = var.winrm_username
       password = var.winrm_password
-      host     = local.ip_address
+      host     = one(flatten([for interface in data.libvirt_domain_interface_addresses.example.interfaces : [for addr in interface.addrs : addr.addr if addr.type == "ipv4" && addr.addr != "127.0.0.1"]]))
       timeout  = "1h"
     }
   }
-  lifecycle {
-    ignore_changes = [
-      nvram,
-      disk[0].wwn,
-      disk[1].wwn,
-    ]
-  }
+}
+
+# TODO https://github.com/dmacvicar/terraform-provider-libvirt/issues/1323
+data "libvirt_domain_interface_addresses" "example" {
+  domain = libvirt_domain.example.name
+  source = "agent"
 }
 
 output "ip" {
-  value = local.ip_address
+  value = one(flatten([for interface in data.libvirt_domain_interface_addresses.example.interfaces : [for addr in interface.addrs : addr.addr if addr.type == "ipv4" && addr.addr != "127.0.0.1"]]))
 }
